e8b298915b177832728495f264f7a1d2dc8b7da8
chunshouyicc-max <chunshouyicc@gmail.com>
2025-11-15 14:24:20 +0800

    Add files via upload

--- game_03.py
+++ game_03.py
@@ -14,22 +14,22 @@ class Card:
         self.name=name
         self.age=age
         self.__password=password#创建私有属性
-    def get_user():
-        return f'用户姓名：{self.name}\n用户年龄：{self.age}'
+    def get_user(self):
+        return f"用户姓名：{self.name} 用户年龄：{self.age}"
     #定义一个私有方法
     def __set_password(self,new_password):
-        self.password=new_password
-        return f'密码已修改为:{self.password}'
+        self.__password=new_password
+        return f'密码已修改为:{self.__password}'
     #定义一个公有函数间接访问私有函数
-    def set_password(self,my_name,newpassword):
+    def set_password(self,my_name,new_password):
         #通过名字作出判断是否能调用修改密码的私有函数
         if my_name==self.name:
-            return __set_password(newpassword)
+            return self.__set_password(new_password)
         else:
             return ('用户名称错误！')
         
 #实例化对象来测试
 card_1=Card('Chen',19,131499)
 print(card_1.get_user())
-print(card_1.setpassword('Chen',246801))
-print(card_1.setpassword('tjy',246801))
+print(card_1.set_password('Chen',246801))
+print(card_1.set_password('tjy',246801))
